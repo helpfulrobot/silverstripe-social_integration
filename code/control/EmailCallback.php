e16cc4bfcdb6acb183fb713c4bf5ed3a88f2e2e7
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-08-26 11:52:38 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/social_integration/branches/2.5@5177 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/control/EmailCallback.php
+++ code/control/EmailCallback.php
@@ -2,9 +2,9 @@
 
 class EmailCallback extends SocialIntegrationControllerBaseClass implements SocialIntegrationAPIInterface {
 
-	public static set_number_of_friends_that_can_be_retrieved($i) {self::$number_of_friends_that_can_be_retrieved = $i;}
-	public static get_number_of_friends_that_can_be_retrieved() {return self::$number_of_friends_that_can_be_retrieved;}
-	public static get_updates() {return null;}
+	public static function set_number_of_friends_that_can_be_retrieved($i) {self::$number_of_friends_that_can_be_retrieved = $i;}
+	public static function get_number_of_friends_that_can_be_retrieved() {return self::$number_of_friends_that_can_be_retrieved;}
+	public static function get_updates($lastNumber) {return null;}
 	/**
 	 * make sure to return TRUE as response if the message is sent
 	 * successfully
